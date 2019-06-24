76fe31beb3de4534b0a3e2040b959bec4081f268
Yanbo Liang <git@liangyanbo.com>
2019-06-25 04:30:25 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/TestClientFilter.java
@@ -56,7 +56,7 @@ public class TestClientFilter implements Filter {
             rttMap.compute(key, (k, v) -> {
                 v.accumulateAndGet(rtt, (old, param) -> {
                     if (old > 0) {
-                        return (long) (0.7 * old + 0.3 * param);
+                        return (long) (0.8 * old + 0.2 * param);
                     } else {
                         return param;
                     }
