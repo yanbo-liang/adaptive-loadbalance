e416ed9df9c6c370884efd6aaca5e8e57a40930d
Yanbo Liang <git@liangyanbo.com>
2019-06-28 04:25:30 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -65,7 +65,7 @@ public class UserLoadBalance implements LoadBalance {
                     minIndex = i;
                 }
             }
-            int change = 20;
+            int change = 35;
             weightArray[minIndex] += change;
 
             while (change > 0) {
