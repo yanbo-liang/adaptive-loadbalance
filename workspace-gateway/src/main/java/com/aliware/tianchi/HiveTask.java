719d0d354d9e70490407a5b7c7f9a22abcf24ee9
Yanbo Liang <git@liangyanbo.com>
2019-07-01 14:36:38 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
@@ -38,7 +38,7 @@ public class HiveTask implements Runnable {
                     .sorted(Comparator.comparingLong(x -> x.averageRtt))
                     .collect(Collectors.toList());
             try {
-                Thread.sleep(300);
+                Thread.sleep(800);
             } catch (Exception e) {
                 e.printStackTrace();
             }
