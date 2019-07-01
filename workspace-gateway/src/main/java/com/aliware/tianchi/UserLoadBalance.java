7706586f6ff840f7c711ea91526396a04906f135
Yanbo Liang <git@liangyanbo.com>
2019-07-01 20:34:29 +0800

    go back to test

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -54,21 +54,21 @@ public class UserLoadBalance implements LoadBalance {
             }
             weightArray[i] = (int) sortedInfo.get(i).maxRequest * (subWeight - i);
 //            weightArray[i] = subWeight - i;
-            HiveInvokerInfo targetInfo = sortedInfo.get(i);
-            long l = averageRttCache(targetInfo);
-            if (targetInfo.averageRttCache != -1) {
-                if (l < targetInfo.averageRttCache * 1.3) {
-                    weightArray[i] = (int) targetInfo.maxRequest;
-                }
-            }
-            targetInfo.averageRttCache = l;
+//            HiveInvokerInfo targetInfo = sortedInfo.get(i);
+//            long l = averageRttCache(targetInfo);
+//            if (targetInfo.averageRttCache != -1) {
+//                if (l < targetInfo.averageRttCache * 1.3) {
+//                    weightArray[i] = (int) targetInfo.maxRequest;
+//                }
+//            }
+//            targetInfo.averageRttCache = l;
 
         }
 
         int index = pickByWeight(weightArray);
         HiveInvokerInfo a = sortedInfo.get(index);
 
-        if (a.currentRequest.get() < (long) (a.maxRequest)) {
+        if (a.currentRequest.get() < (long) (a.maxRequest*0.8)) {
             return a.invoker;
         }
         for (int i = 0; i < invokers.size(); i++) {
@@ -76,7 +76,7 @@ public class UserLoadBalance implements LoadBalance {
             if (hiveInvokerInfo == a) {
                 continue;
             }
-            if (hiveInvokerInfo.currentRequest.get() < (long) (hiveInvokerInfo.maxRequest)) {
+            if (hiveInvokerInfo.currentRequest.get() < (long) (hiveInvokerInfo.maxRequest*0.8)) {
 
                 return hiveInvokerInfo.invoker;
 
