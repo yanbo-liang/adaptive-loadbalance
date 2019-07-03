d6c6bb320057153d07a2be2b216fde191fe99ea7
Yanbo Liang <git@liangyanbo.com>
2019-07-03 19:33:58 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -34,14 +34,18 @@ public class UserLoadBalance implements LoadBalance {
             }
         }
 
-        List<HiveInvokerInfo> sortedInfo = infos.stream().sorted(Comparator.comparingLong(x ->
-                (long) Arrays.stream(x.rttCache).max().orElse(0)))
+        List<HiveInvokerInfo> sortedInfo = infos.stream().sorted(Comparator.comparingLong(x -> {
+            long a = (long) Arrays.stream(x.rttCache).max().orElse(0);
+            System.out.println(a);
+            return a;
+        }))
                 .collect(Collectors.toList());
+
 
         int[] weightArray = new int[sortedInfo.size()];
         int subWeight = sortedInfo.size();
         for (int i = 0; i < sortedInfo.size(); i++) {
-            weightArray[i] = (int) sortedInfo.get(i).maxRequest /13 * (13 + subWeight - i - 1);
+            weightArray[i] = (int) sortedInfo.get(i).maxRequest / 13 * (13 + subWeight - i - 1);
 //            weightArray[i] = sortedInfo.size()-i;
         }
 
