fcad8ebfc8a82d4f51414ca0a29e5338793206f3
Yanbo Liang <git@liangyanbo.com>
2019-07-04 01:12:14 +0800

    搞探测

--- workspace-gateway/src/main/java/com/aliware/tianchi/HiveInvokerInfo.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/HiveInvokerInfo.java
@@ -20,7 +20,7 @@ public class HiveInvokerInfo {
 
     volatile double stressCoefficient = 0.5;
 
-    long[] rttCache = new long[30];
+    long[] rttCache = new long[20];
     AtomicInteger rttCacheIndex = new AtomicInteger(-1);
 //    final ReadWriteLock lock = new ReentrantReadWriteLock();
 
