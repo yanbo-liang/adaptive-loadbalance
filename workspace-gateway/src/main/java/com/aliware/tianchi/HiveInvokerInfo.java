ebf6ab118fba676d4500b0ce9f6929f520fe826d
Yanbo Liang <git@liangyanbo.com>
2019-07-03 13:43:50 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/HiveInvokerInfo.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/HiveInvokerInfo.java
@@ -13,7 +13,7 @@ public class HiveInvokerInfo {
     volatile long maxRequest = 0;
     AtomicLong currentRequest = new AtomicLong(0);
 
-    static int length = 50;
+    static int length = 500;
     long[] rttCache = new long[length];
     AtomicInteger rttCacheIndex = new AtomicInteger(-1);
      final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
