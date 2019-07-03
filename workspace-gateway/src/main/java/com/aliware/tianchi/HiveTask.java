7f51396d5d37a357019835bf9d4b58c67538fd4a
Yanbo Liang <git@liangyanbo.com>
2019-07-04 01:35:09 +0800

    搞探测

--- workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
@@ -13,7 +13,7 @@ public class HiveTask implements Runnable {
                 List<HiveInvokerInfo> values = new ArrayList<>(UserLoadBalance.infoMap.values());
                 for (int i = 0; i < values.size(); i++) {
                     HiveInvokerInfo hiveInvokerInfo = values.get(i);
-                    int lastAverage = 0;
+                    int totalAverage = 0;
                     hiveInvokerInfo.stressCoefficient = 0.5;
                     UserLoadBalance.stressInvokerInfo = hiveInvokerInfo;
                     for (int j = 0; j < 25; j++) {
@@ -29,15 +29,14 @@ public class HiveTask implements Runnable {
                         int average = hiveInvokerInfo.rttTotalTime.get() / hiveInvokerInfo.rttTotalCount.get();
                         hiveInvokerInfo.rttTotalTime.updateAndGet(x -> 0);
                         hiveInvokerInfo.rttTotalCount.updateAndGet(x -> 0);
-                        if (lastAverage == 0) {
-                            lastAverage = average;
-                        } else {
-                            if (average > lastAverage * 1.8) {
+
+                        if (j != 0) {
+                            if (average > totalAverage / (j) * 1.8) {
                                 break;
-                            } else {
-                                lastAverage = average;
                             }
                         }
+                        totalAverage += average;
+
                         hiveInvokerInfo.stressCoefficient += 0.02;
                     }
                 }
