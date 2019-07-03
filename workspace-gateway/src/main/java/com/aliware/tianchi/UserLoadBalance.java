8009da618b40f0ab38450b10a5c9e198734bd446
Yanbo Liang <git@liangyanbo.com>
2019-07-03 16:18:06 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -60,20 +60,21 @@ public class UserLoadBalance implements LoadBalance {
 //        HiveInvokerInfo pickedInvoker = sortedInfo.get(0);
 
 //        for (HiveInvokerInfo pickedInvoker : sortedInfo) {
-        if (pickedInvoker.currentRequest.get() < pickedInvoker.maxRequest) {
-            return pickedInvoker.invoker;
-        }
-        for (int i = 0; i < invokers.size(); i++) {
-            HiveInvokerInfo hiveInvokerInfo = sortedInfo.get(i);
-            if (hiveInvokerInfo.invoker.getUrl().equals(pickedInvoker.invoker.getUrl())) {
-                continue;
-            }
-            if (hiveInvokerInfo.currentRequest.get() < hiveInvokerInfo.maxRequest) {
-                return hiveInvokerInfo.invoker;
-            }
-        }
+//        if (pickedInvoker.currentRequest.get() < pickedInvoker.maxRequest) {
+//            return pickedInvoker.invoker;
 //        }
-        return randomInvoker;
+//        for (int i = 0; i < invokers.size(); i++) {
+//            HiveInvokerInfo hiveInvokerInfo = sortedInfo.get(i);
+//            if (hiveInvokerInfo.invoker.getUrl().equals(pickedInvoker.invoker.getUrl())) {
+//                continue;
+//            }
+//            if (hiveInvokerInfo.currentRequest.get() < hiveInvokerInfo.maxRequest) {
+//                return hiveInvokerInfo.invoker;
+//            }
+//        }
+////        }
+//        System.out.println("asdsadsad");
+        return pickedInvoker.invoker;
 
 //
 //        List<HiveInvokerInfo> sortedInfo = HiveTask.sortedInfo;
