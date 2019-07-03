566c8383c847bafbd9b86066f5ab3e03549fddb4
Yanbo Liang <git@liangyanbo.com>
2019-07-04 00:29:49 +0800

    搞探测

--- workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
@@ -8,40 +8,42 @@ public class HiveTask implements Runnable {
     @Override
     public void run() {
         try {
-            List<HiveInvokerInfo> values = new ArrayList<>(UserLoadBalance.infoMap.values());
-            for (int i = 0; i < values.size(); i++) {
-                HiveInvokerInfo hiveInvokerInfo = values.get(i);
-                int lastAverage = 0;
-                hiveInvokerInfo.stressCoefficient = 0.5;
-                UserLoadBalance.stressInvokerInfo = hiveInvokerInfo;
-                for (int j = 0; j < 50; j++) {
-                    UserLoadBalance.stress = true;
-                    HiveFilter.stress = true;
-                    Thread.sleep(1);
-                    UserLoadBalance.stress = false;
-                    HiveFilter.stress = false;
-                    int rttTotalCount= hiveInvokerInfo.rttTotalCount.get();
-                    if (rttTotalCount==0){
-                        continue;
-                    }
-                    int average = hiveInvokerInfo.rttTotalTime.get() / hiveInvokerInfo.rttTotalCount.get();
-                    hiveInvokerInfo.rttTotalTime.updateAndGet(x -> 0);
-                    hiveInvokerInfo.rttTotalCount.updateAndGet(x -> 0);
-                    if (lastAverage == 0) {
-                        lastAverage = average;
-                    } else {
-                        if (average > lastAverage * 1.1) {
-                            break;
+            while (true) {
+                List<HiveInvokerInfo> values = new ArrayList<>(UserLoadBalance.infoMap.values());
+                for (int i = 0; i < values.size(); i++) {
+                    HiveInvokerInfo hiveInvokerInfo = values.get(i);
+                    int lastAverage = 0;
+                    hiveInvokerInfo.stressCoefficient = 0.5;
+                    UserLoadBalance.stressInvokerInfo = hiveInvokerInfo;
+                    for (int j = 0; j < 50; j++) {
+                        UserLoadBalance.stress = true;
+                        HiveFilter.stress = true;
+                        Thread.sleep(1);
+                        UserLoadBalance.stress = false;
+                        HiveFilter.stress = false;
+                        int rttTotalCount = hiveInvokerInfo.rttTotalCount.get();
+                        if (rttTotalCount == 0) {
+                            continue;
                         }
+                        int average = hiveInvokerInfo.rttTotalTime.get() / hiveInvokerInfo.rttTotalCount.get();
+                        hiveInvokerInfo.rttTotalTime.updateAndGet(x -> 0);
+                        hiveInvokerInfo.rttTotalCount.updateAndGet(x -> 0);
+                        if (lastAverage == 0) {
+                            lastAverage = average;
+                        } else {
+                            if (average > lastAverage * 1.1) {
+                                break;
+                            }
+                        }
+                        hiveInvokerInfo.stressCoefficient += 0.01;
                     }
-                    hiveInvokerInfo.stressCoefficient += 0.01;
                 }
+                System.out.println();
+                for (HiveInvokerInfo info : values) {
+                    System.out.println(info.stressCoefficient);
+                }
+                Thread.sleep(2000);
             }
-            System.out.println();
-            for (HiveInvokerInfo info:values){
-                System.out.println(info.stressCoefficient);
-            }
-            Thread.sleep(2000);
         } catch (
                 Exception e) {
             e.printStackTrace();
