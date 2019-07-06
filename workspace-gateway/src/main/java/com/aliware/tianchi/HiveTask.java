d507b7f25dfa1a363c52e289976fe91004c659db
Yanbo Liang <git@liangyanbo.com>
2019-07-07 01:00:18 +0800

    搞概率

--- workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/HiveTask.java
@@ -68,7 +68,7 @@ public class HiveTask implements Runnable {
                     }
                 }
                 try {
-                    Thread.sleep(5);
+                    Thread.sleep(50);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
