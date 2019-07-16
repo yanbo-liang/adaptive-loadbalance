5b27224effebb330fa09fafca4bfeb4f158d6906
Yanbo Liang <git@liangyanbo.com>
2019-07-16 13:44:30 +0800

    搞概率

--- workspace-gateway/src/main/java/com/aliware/tianchi/HiveCommon.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/HiveCommon.java
@@ -140,9 +140,9 @@ public class HiveCommon {
         double belowWeight = belowList.stream().mapToDouble(x -> x.weight).sum();
         double weightChange;
         if (belowWeight > aboveWeight) {
-            weightChange = aboveWeight * 0.04;
+            weightChange = aboveWeight * 0.05;
         } else {
-            weightChange = belowWeight * 0.02;
+            weightChange = belowWeight * 0.05;
         }
         logger.info("{}-{}--{}", format.format(date), weightedRttAverage, weightChange);
 
